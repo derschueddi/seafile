63a68c5e97522a56eb6df5296b8724cfd169d673
Shuai Lin <linshuai2012@gmail.com>
2016-03-05 18:39:54 +0800

    Updated seahub thirdparty libs in travis-ci scripts.

--- integration-tests/install-deps.sh
+++ integration-tests/install-deps.sh
@@ -35,22 +35,27 @@ thirdpart=$HOME/thirdpart
 
 mkdir -p $downloads && cd $downloads
 urls=(
-    http://pypi.python.org/packages/source/g/gunicorn/gunicorn-0.16.1.tar.gz
-    http://pypi.python.org/packages/source/f/flup/flup-1.0.tar.gz
+    https://www.djangoproject.com/m/releases/1.8/Django-1.8.10.tar.gz
+    https://pypi.python.org/packages/source/d/django-statici18n/django-statici18n-1.1.3.tar.gz
+    https://pypi.python.org/packages/source/d/djangorestframework/djangorestframework-3.3.2.tar.gz
+    https://pypi.python.org/packages/source/d/django_compressor/django_compressor-1.4.tar.gz
+
+    https://pypi.python.org/packages/source/j/jsonfield/jsonfield-1.0.3.tar.gz
+    https://pypi.python.org/packages/source/d/django-post_office/django-post_office-2.0.6.tar.gz
+
+    http://pypi.python.org/packages/source/g/gunicorn/gunicorn-19.4.5.tar.gz
+    http://pypi.python.org/packages/source/f/flup/flup-1.0.2.tar.gz
     https://pypi.python.org/packages/source/c/chardet/chardet-2.3.0.tar.gz
     https://labix.org/download/python-dateutil/python-dateutil-1.5.tar.gz
     https://pypi.python.org/packages/source/s/six/six-1.9.0.tar.gz
-    https://pypi.python.org/packages/source/d/django-statici18n/django-statici18n-1.1.3.tar.gz
-    https://pypi.python.org/packages/source/d/django_compressor/django_compressor-1.4.tar.gz
+
     https://pypi.python.org/packages/source/d/django-picklefield/django-picklefield-0.3.2.tar.gz
     https://pypi.python.org/packages/source/d/django-constance/django-constance-1.0.1.tar.gz
+
     https://pypi.python.org/packages/source/j/jdcal/jdcal-1.2.tar.gz
     https://pypi.python.org/packages/source/e/et_xmlfile/et_xmlfile-1.0.1.tar.gz
     https://pypi.python.org/packages/source/o/openpyxl/openpyxl-2.3.0.tar.gz
 )
-# The basename of the download url is different from the tarball name.
-[[ -e Django-1.5.12.tar.gz ]] || $WGET https://www.djangoproject.com/download/1.5.12/tarball -O Django-1.5.12.tar.gz
-[[ -e djblets-0.6.14.tar.gz ]] || $WGET https://github.com/djblets/djblets/tarball/release-0.6.14 -O djblets-0.6.14.tar.gz
 for url in ${urls[*]}; do
     [[ -e $(basename $url) ]] || $WGET $url
 done
